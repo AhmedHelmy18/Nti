a968eee020a493bd88421e544afde23f36299347
AhmedHelmy18 <ahmedmohammed18112003@gmail.com>
2025-05-27 23:03:34 +0300

    done seventh task.

---
+++ task7.dart
@@ -0,0 +1,15 @@
+void main() {
+  for (int i = 1; i <= 20; i++) {
+    print(i);
+  }
+
+  print(
+    "---------------------------------------------------------------------",
+  );
+
+  List<String> products = ["لابتوب", "هاتف", "ساعة"];
+
+  for (int i = 0; i < products.length; i++) {
+    print(products[i]);
+  }
+}
