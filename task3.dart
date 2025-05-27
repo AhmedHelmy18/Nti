2472985b522533fdbb4b86811f167b58e379502c
AhmedHelmy18 <ahmedmohammed18112003@gmail.com>
2025-05-27 22:06:48 +0300

    done third task.

---
+++ task3.dart
@@ -0,0 +1,13 @@
+void main() {
+  List<String> fruits = ["Apple", "Banana", "Orange"];
+  print(fruits);
+
+  fruits.add("Mango");
+  print(fruits);
+
+  fruits.remove("Apple");
+  print(fruits);
+
+  fruits[1] = "Watermelon";
+  print(fruits);
+}
